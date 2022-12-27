a5af95d4915f07302242fa4ce5180c8dfdd54826
vktyfgfh <44190495+vktyfgfh@users.noreply.github.com>
2022-12-27 10:15:52 +0900

    Update 7.prac.py

--- 7.prac.py
+++ 7.prac.py
@@ -199,7 +199,7 @@ with col3:
 
 
 # slider를 사용하여 구간 설정하기
-values = st.slider('가격을 선택하세요', min, max, avg, 5)
-st.write('Values:', values)
+values = st.slider('가격을 선택하세요', avg * 8/12, avg* 0.92, avg, 5)
+st.write('선택한 값 :', values)
 
 # 파일실행: File > New > Terminal(anaconda prompt) - streamlit run streamlit\7.prac_ans.py
