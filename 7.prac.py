08691f720669cad42e13bc1be279e11261e94280
vktyfgfh <44190495+vktyfgfh@users.noreply.github.com>
2022-12-27 15:50:49 +0900

    Update 7.prac.py

--- 7.prac.py
+++ 7.prac.py
@@ -103,8 +103,7 @@ st.subheader('농가면적 대비 잔존량')
 # 경상북도 시과 전체 농지
 t_hr = df_output[df_output['경상북도']==last_year]['사과면적 (ha)']*100
 farm_hr = st.number_input('농가면적을 입력하시기 바랍니다.(a)')
-st.write(farm_hr)
-f_remain = t_remain * farm_hr/t_hr
+f_remain = int(t_remain * farm_hr/t_hr)
 st.write('농가예상잔존량 (kg) :', f_remain)
 
 st.subheader('사과 적정가격 범위')
