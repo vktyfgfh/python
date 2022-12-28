45589f9dfe37cd76d174d60499c40e938df3f16b
vktyfgfh <44190495+vktyfgfh@users.noreply.github.com>
2022-12-28 14:08:01 +0900

    Update 7.prac.py

--- 7.prac.py
+++ 7.prac.py
@@ -148,8 +148,31 @@ with col3:
     st.write(int(avg* 0.92))
     
 #slider를 사용하여 구간 설정하기
-s = st.slider('저희가 제안하는 사과 적정가격 (빨간부분) ', 11000, 18000, (12369, 17069))
-s1 = st.slider("가격을 선택하세요", min_value=12369, max_value=17069, value=14719)
+s = st.slider('제안하는 사과 적정가격 범위 ', 11000, 18000, (12369, 17069))
+s1 = st.slider("희망 판매 가격을 선택하세요", min_value=12369, max_value=17069, value=14719)
+
+NB = st.sidebar.select_slider('', options = [1,10,20,30,40,50,60,70,80,90,100], value = 1)
+
+
+ColorMinMax = st.markdown(''' <style> div.stSlider > div[data-baseweb = "slider"] > div[data-testid="stTickBar"] > div {
+    background: rgb(1 1 1 / 0%); } </style>''', unsafe_allow_html = True)
+
+
+Slider_Cursor = st.markdown(''' <style> div.stSlider > div[data-baseweb="slider"] > div > div > div[role="slider"]{
+    background-color: rgb(14, 38, 74); box-shadow: rgb(14 38 74 / 20%) 0px 0px 0px 0.2rem;} </style>''', unsafe_allow_html = True)
+
+    
+Slider_Number = st.markdown(''' <style> div.stSlider > div[data-baseweb="slider"] > div > div > div > div
+                                { color: rgb(14, 38, 74); } </style>''', unsafe_allow_html = True)
+    
+
+col = f''' <style> div.stSlider > div[data-baseweb = "slider"] > div > div {{
+    background: linear-gradient(to right, rgb(1, 183, 158) 0%, 
+                                rgb(1, 183, 158) {NB}%, 
+                                rgba(151, 166, 195, 0.25) {NB}%, 
+                                rgba(151, 166, 195, 0.25) 100%); }} </style>'''
+
+ColorSlider = st.markdown(col, unsafe_allow_html = True)
 
 st.write(' ')
 st.write(' ')
