d01a935331d5c3ddf04026c2532626236fcf9256
vktyfgfh <44190495+vktyfgfh@users.noreply.github.com>
2023-01-02 10:23:47 +0900

    Update 7.prac.py

--- 7.prac.py
+++ 7.prac.py
@@ -57,7 +57,8 @@ col1, col2 = st.columns(2)
 with col1:
     st.text('작년 사과 총 생산량')
     output = df_output[df_output['경상북도']== last_year]['생산량 (톤)']
-    st.write(output*1000)
+    output['생산량 (kg)'] = output['생산량 (톤)'] * 1000    
+    st.write(output['생산량 (kg)'])
 with col2:
     st.text('작년 경북 사과 생산량 (kg)')
     output = df_output[df_output['경상북도']==last_year]['사과면적 (ha)'] * df_output[df_output['경상북도']==last_year]['10a당 생산량 (kg)'] * 10
